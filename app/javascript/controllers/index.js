701c1b6a9ed111c9cd8610c75dfde4a8ac18e9d8
Clari Ursini <clari_ursini@hotmail.com>
2022-11-01 00:48:14 -0300

    updated application.js to do some tests, erased afterwards

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,11 @@
 
 import { application } from "./application"
 
+import ChatBroadcastController from "./chat_broadcast_controller"
+application.register("chat-broadcast", ChatBroadcastController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ResetFormController from "./reset_form_controller"
+application.register("reset-form", ResetFormController)
