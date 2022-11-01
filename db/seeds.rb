4416330d1492f341a765edf9182c3e4445dd70b5
Clari Ursini <clari_ursini@hotmail.com>
2022-11-01 16:32:28 -0300

    updated seeds to add more users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,30 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require "open-uri"
+
 User.destroy_all
 puts "Destroyed Users"
+
 puts "Creatting Users"
-  user1 = User.create!(email: "clari@gmail.com", password: "123456")
-  user2 = User.create!(email: "nacho@gmail.com", password: "123456")
-  user3 = User.create!(email: "ramiro@gmail.com", password: "123456")
-  user4 = User.create!(email: "pedro@gmail.com", password: "123456")
+user1 = User.new(email: "clari@gmail.com", password: "123456", name: "Clari", age: 34, description: "I love dogs")
+file = URI.open("https://ca.slack-edge.com/T02NE0241-U03PQPPGXUG-abbe6fa51dcc-512")
+user1.photo.attach(io: file, filename: "clari.png", content_type: "image/png")
+user1.save!
+
+user2 = User.new(email: "nacho@gmail.com", password: "123456", name: "Nacho", age: 27, description: "I love programming")
+file = URI.open("https://ca.slack-edge.com/T02NE0241-U03NVDNP02Z-8e39c56cb377-512")
+user2.photo.attach(io: file, filename: "nacho.png", content_type: "image/png")
+user2.save!
+
+user3 = User.new(email: "sofi@gmail.com", password: "123456", name: "Sofi", age: 32, description: "I love to travel")
+file = URI.open("https://ca.slack-edge.com/T02NE0241-U03NNS0RVHV-360467752b45-512")
+user3.photo.attach(io: file, filename: "sofi.png", content_type: "image/png")
+user3.save!
+
+user4 = User.new(email: "pedro@gmail.com", password: "123456", name: "Pedro", age: 25, description: "I love cars")
+file = URI.open("https://ca.slack-edge.com/T02NE0241-USKLA4JSG-b4bc4a5daadb-512")
+user4.photo.attach(io: file, filename: "sofi.png", content_type: "image/png")
+user4.save!
+
 puts "Created users"
