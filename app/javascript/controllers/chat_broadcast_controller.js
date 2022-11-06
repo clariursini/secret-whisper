b8c0d09965c8e2ce883711990f1e4cfe26310667
Clari Ursini <clari_ursini@hotmail.com>
2022-11-06 15:43:19 -0300

    updated chat broadcast controller, added scroll down, reset form and unsubscribe

--- app/javascript/controllers/chat_broadcast_controller.js
+++ app/javascript/controllers/chat_broadcast_controller.js
@@ -8,9 +8,23 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatChannel", id: this.conversationIdValue },
-      { received: data => console.log(data) }
+      { received: data => this.#insertMessageAndScrollDown(data)}
     )
 
     console.log(`Subscribe to the conversation with the id ${this.conversationIdValue}.`)
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
   }
 }
