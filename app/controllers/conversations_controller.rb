942eb874880955d1e5e5aad15e0f2e64e71f903d
Clari Ursini <clari_ursini@hotmail.com>
2022-11-06 15:41:31 -0300

    updated conversations controller to add conversation new in the index

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,10 +2,11 @@ class ConversationsController < ApplicationController
   def index
     @users = User.all
     @conversations = Conversation.all
+    @conversation = Conversation.new
   end
 
   def create
-    if Conversation.between(params[:sender_id],params[:recipient_id]).present?
+    if Conversation.between(params[:sender_id], params[:recipient_id]).present?
       @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
     else
       @conversation = Conversation.create!(conversation_params)
