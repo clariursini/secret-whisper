6064de05df0b4065018e0a02b2378ce2d214be45
Clari Ursini <clari_ursini@hotmail.com>
2022-11-07 15:49:17 -0300

    added remember me to permited parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :description, :age, :photo ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :description, :age, :photo, :remember_me ])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :description, :age, :photo ])
-
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :description, :age, :photo, :remember_me ])
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(*)
     # stored_location_for(resource) || welcome_path
     friends_path
   end
