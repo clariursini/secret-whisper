509c9f914d82cc29cdbff631991af334427ccf60
Clari Ursini <clari_ursini@hotmail.com>
2022-11-07 18:46:19 -0300

    added over ten variable

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,6 +9,22 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.find(params[:id])
     @messages = @conversation.messages
     @message = Message.new
+
+    if @messages.length > 10
+      @over_ten = true
+      @messages = @messages[-10..-1]
+    end
+
+    if params[:m]
+      @over_ten = false
+      @messages = @conversation.messages
+    end
+
+    if @messages.last
+      if @messages.last.user_id != current_user.id
+        @messages.last.read = true
+      end
+    end
   end
 
   def create
