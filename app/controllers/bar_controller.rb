bdde5d23250c2316b4a7f1e6609aabf08f1eb194
Yuchi Hsueh <bhsueh94@gmail.com>
2022-11-10 18:20:10 -0300

    add links for barcode changing

--- app/controllers/bar_controller.rb
+++ app/controllers/bar_controller.rb
@@ -3,4 +3,11 @@ class BarController < ApplicationController
     @bar = Bar.find(current_user.bar_code)
     @users = User.where(bar_code: current_user.bar_code)
   end
+
+  def barcode
+    @user = User.find(current_user.id)
+    @user.bar_code = params[:id]
+    @user.save
+    redirect_to bar_path
+  end
 end
