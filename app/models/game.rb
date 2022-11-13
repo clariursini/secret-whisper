a2567767c45685562e8eb4a41d2b23a7f433fe3c
Clari Ursini <clari_ursini@hotmail.com>
2022-11-13 09:42:52 -0300

    added game model

---
+++ app/models/game.rb
@@ -0,0 +1,11 @@
+class Game < ApplicationRecord
+  belongs_to :player1, :foreign_key => :player1_id, class_name: 'User'
+  belongs_to :player2, :foreign_key => :player2_id, class_name: 'User'
+  belongs_to :winner, :foreign_key => :winner_id, class_name: 'User'
+
+  validates_uniqueness_of :player1_id, :scope => :player2_id
+
+  scope :between, -> (player1_id, player2_id) do
+    where("(games.player1_id = ? AND games.player2_id =?) OR (games.player1_id = ? AND games.player2_id =?)", player1_id, player2_id, player2_id, player1_id)
+  end
+end
