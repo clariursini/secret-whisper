1cf486a7c553d95cb1f408cc7c613bdba0b64548
Clari Ursini <clari_ursini@hotmail.com>
2022-11-14 00:54:33 -0300

    updating game controller for creating game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,15 +9,28 @@ class GamesController < ApplicationController
   end
 
   def new
-    @game = Game.new(game_params)
+    @game = Game.new
   end
 
   def create
-    if Game.between(params[:player1_id], params[:player2_id]).present?
-      @game = Game.between(params[:player1_id], params[:player2_id]).first
+    @player1 = User.find(params[:player1_id]).id
+    @player2 = User.find(params[:player2_id]).id
+    @game = Game.new(game_params)
+
+    @game.player1_id = @player1
+    @game.player2_id = @player2
+
+    if @game.save!
+      redirect_to bar_path(current_user.bar_code)
     else
-      @game = Game.create!(game_params)
+      render :new, status: :unprocessable_entity
     end
+
+    # if Game.between(params[:player1_id], params[:player2_id]).present?
+    #   @game = Game.between(params[:player1_id], params[:player2_id]).first
+    # else
+    #   @game = Game.create!(game_params)
+    # end
   end
 
   def edit
@@ -26,14 +39,39 @@ class GamesController < ApplicationController
 
   def update
     @game = Game.find(params[:id])
-    @game.update(game_params)
 
-    redirect_to game_path(@game)
+    if @game.hand1 == "Paper" && @game.hand2 == "Rock"
+      @game.winner_id = @game.player1_id
+    elsif @game.hand1 == "Paper" && @game.hand2 == "Scissors"
+      @game.winner_id = @game.player2_id
+    elsif @game.hand1 == "Paper" && @game.hand2 == "Paper"
+      @game.winner_id = @game.player1_id
+
+    elsif @game.hand1 == "Rock" && @game.hand2 == "Paper"
+      @game.winner_id = @game.player2_id
+    elsif @game.hand1 == "Rock" && @game.hand2 == "Scissors"
+      @game.winner_id = @game.player1_id
+    elsif @game.hand1 == "Rock" && @game.hand2 == "Rock"
+      @game.winner_id = @game.player1_id
+
+    elsif @game.hand1 == "Scissors" && @game.hand2 == "Paper"
+      @game.winner_id = @game.player1_id
+    elsif @game.hand1 == "Scissors" && @game.hand2 == "Rock"
+      @game.winner_id = @game.player2_id
+    elsif @game.hand1 == "Scissors" && @game.hand2 == "Scissors"
+      @game.winner_id = @game.player1_id
+
+    end
+
+    if @game.update(game_params)
+      redirect_to game_path(@game)
+    end
+
   end
 
   private
 
   def game_params
-    params.permit(:player1_id, :player2_id, :winner_id)
+    params.require(:game).permit(:hand1, :hand2)
   end
 end
