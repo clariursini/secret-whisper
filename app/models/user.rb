7567aae5ac10d1cc3b64df469b8a424ccf9c39b5
Clari Ursini <clari_ursini@hotmail.com>
2022-11-19 13:05:30 -0300

    added validation of presence for name and gender

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
+
+  validates_presence_of :name, :gender
 end
