882e44d6951c514a218bc768ae61b1df9162b677
Yuchi Hsueh <bhsueh94@gmail.com>
2022-11-28 20:49:20 -0300

    fix barshow buttons & update post

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,6 @@
 class BeersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @beers = Beer.where(recipient_id: current_user).order(created_at: :desc)
     @beers.each do |beer|
@@ -19,12 +21,20 @@ class BeersController < ApplicationController
   def destroy
     @beerTarget = Beer.find(params[:id])
     @beerTarget.destroy
+  end
 
-    # if Beer.between(params[:sender_id], params[:recipient_id]).present?
-    #   @beer = Beer.between(params[:sender_id], params[:recipient_id]).destroy
-    # else
-    #   @beer = Beer.between(params[:sender_id], params[:recipient_id]).first
-    # end
+  def create_beer
+    @beer = Beer.new(sender_id: current_user.id, recipient_id: params[:id])
+    if @beer.save
+      redirect_to beers_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def delete_beer
+    @beer = Beer.find(params[:id])
+    @beer.destroy
   end
 
   private
