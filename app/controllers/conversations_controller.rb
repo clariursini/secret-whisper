32cb26fd09599af724e6a77f0e4f2ef8b2aa15f7
Clari Ursini <clari_ursini@hotmail.com>
2022-12-26 10:25:02 -0300

    added conversation order method, removed over ten

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,17 @@ class ConversationsController < ApplicationController
   def index
     @users = User.all
     @conversations = Conversation.all
+
+    @conv = @conversations.select { |conv| conv.messages.last != nil}
+    @conv_ordered = @conv.sort_by { |conv| conv.messages.last.created_at }
+    @conv_ordered = @conv_ordered.reverse
+
     @conversation = Conversation.new
+
+    # @conversations.each do |conversation|
+    #   @message_date = conversation.messages.last.created_at
+    #   @conv_ordered = @conversations.sort_by { |conv| conv.messages.last.created_at }
+    # end
   end
 
   def show
@@ -10,21 +20,29 @@ class ConversationsController < ApplicationController
     @messages = @conversation.messages
     @message = Message.new
 
-    if @messages.length > 10
-      @over_ten = true
-      @messages = @messages[-10..-1]
-    end
+    # if @messages.length > 10
+    #   @over_ten = true
+    #   @messages = @messages[-10..-1]
+    # end
 
-    if params[:m]
-      @over_ten = false
-      @messages = @conversation.messages
-    end
+    # if params[:m]
+    #   @over_ten = false
+    #   @messages = @conversation.messages
+    # end
 
-    if @messages.last
-      if @messages.last.user_id != current_user.id
-        @messages.last.read = true
+    # if @messages.last
+    #   if @messages.last.user_id != current_user.id
+    #     @messages.last.read = true
+    #   end
+    # end
+
+    @messages.each do |message|
+      if message.user_id != current_user.id
+        message.read = true
+        message.save!
       end
     end
+
   end
 
   def create
@@ -32,6 +50,8 @@ class ConversationsController < ApplicationController
       @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
     else
       @conversation = Conversation.create!(conversation_params)
+      # @conversation.messages.last = Message.create!(body: ".", user_id: current_user.id, conversation_id: @conversation.id)
+      # @conversation.save!
     end
     redirect_to conversation_path(@conversation)
   end
